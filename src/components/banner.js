4f28230d6d1a3836667402750b528a93d7949991
Regina Battle <hello@reginabattle.com>
2019-05-09 18:59:31 -0400

    Update card component

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,30 +3,18 @@ import couch from '../images/photo-couch.jpg';
 
 const Banner = () => {
 	return (
-		<React.Fragment>
-
-			<section className="banner">
-				<figure className="banner-image" role="img" aria-label="Three kids sitting ona couch playing video games">
-					<figcaption className="banner-caption">
-					  <div className="banner-caption-content">
-							<p>New games and accessories</p>
-							<h2>Monthly packages. <br/> Excitement delivered daily.</h2>
-							<p>What&rsquo;s the best way to shop for the latest video games and peripherals? How about never shopping at all? You’ll get new stuff on your doorstep &mdash; every month.</p>
-							<a href="#" className="button" aria-label="Get started with a monthly subscription">Get started</a>
-						</div>
-					</figcaption>
-				</figure>
-		  </section>
-
-		  <section className="content">
-		  	<h2>How it works</h2>
-
-		  	<div className="info-cards">
-		  	
-		  	</div>
-		  </section>
-
-		</React.Fragment>
+		<section className="banner">
+			<figure className="banner-image" role="img" aria-label="Three kids sitting ona couch playing video games">
+				<figcaption className="banner-caption">
+				  <div className="banner-caption-content">
+						<p>New games and accessories</p>
+						<h2>Monthly packages. <br/> Excitement delivered daily.</h2>
+						<p>What&rsquo;s the best way to shop for the latest video games and peripherals? How about never shopping at all? You’ll get new stuff on your doorstep &mdash; every month.</p>
+						<a href="#" className="button" aria-label="Get started with a monthly subscription">Get started</a>
+					</div>
+				</figcaption>
+			</figure>
+	  </section>
 	)
 }
 
--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Card = () => {
 	return (
+		<section className="info-cards">
+		  <h2>How it works</h2>
 
+		  <div className="cards">
+		  	
+		  </div>
+		</section>
 	)
 }
 
