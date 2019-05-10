dfb698a6328e925b703473dc21629459b49fc787
Regina Battle <hello@reginabattle.com>
2019-05-09 20:00:47 -0400

    Get and display data from API

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import couch from '../images/photo-couch.jpg';
 
 const Banner = () => {
 	return (
@@ -10,7 +9,7 @@ const Banner = () => {
 						<p>New games and accessories</p>
 						<h2>Monthly packages. <br/> Excitement delivered daily.</h2>
 						<p>What&rsquo;s the best way to shop for the latest video games and peripherals? How about never shopping at all? You’ll get new stuff on your doorstep &mdash; every month.</p>
-						<a href="#" className="button" aria-label="Get started with a monthly subscription">Get started</a>
+						<button className="button">Get started</button>
 					</div>
 				</figcaption>
 			</figure>
--- src/components/steps.js
+++ src/components/steps.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
 
-class Cards extends Component {
+class Steps extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			isLoaded: false,
+			steps: []
+		};	
+	}
+
+	componentDidMount() {
+		fetch("https://uqnzta2geb.execute-api.us-east-1.amazonaws.com/default/FrontEndCodeChallenge")
+			.then(response => response.json())
+			.then(json => {
+				this.setState({
+					isLoaded: true,
+					steps: json
+				});
+				console.log('steps:', this.state.steps);
+			}
+		);
+	}
+
 	render() {
+		const { isLoaded, steps } = this.state;
 		return (
-			<section className="steps-container">
+			<section className="steps">
 			  <h2>How it works</h2>
+
+			  <ul className="steps-list">
+          {steps.map(step => (
+            <li key={step.id} className="step">
+              <span className="step-number">{step.stepNumber}</span>
+            </li>
+          ))}
+        </ul>
 			</section>
 		)
 	}
 }
 
-export default Cards;
+export default Steps;
