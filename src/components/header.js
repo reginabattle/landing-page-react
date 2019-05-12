c45149931f489d37552150aa812736d2a91cf009
Regina Battle <hello@reginabattle.com>
2019-05-12 18:01:22 -0400

    Add file back - test

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../images/logo-endless.svg';
+
+const Header = () => {
+	return(
+		<header className="header">
+		  <div className="header-content">
+		  	<h1><img className="logo" src={logo} alt="Endless" /></h1>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
